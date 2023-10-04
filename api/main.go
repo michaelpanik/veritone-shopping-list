928bd24efbdd6522492f7e5f2eea59cb4eedca9a
Michael Panik <michael@michael-panik.com>
2023-10-04 15:08:36 -0500

    Add CORs, get frontend and backend talking

--- api/main.go
+++ api/main.go
@@ -3,11 +3,18 @@ package main
 import (
 	"net/http"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
+
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"http://localhost:8000"}
+
+	r.Use(cors.New(config))
+
 	r.GET("/health", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "Hello, world!",
