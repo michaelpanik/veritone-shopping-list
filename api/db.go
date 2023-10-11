57fd63291e70256f016690acde0cd3c15f21f8d0
Michael Panik <michael@michael-panik.com>
2023-10-10 23:52:48 -0500

    Attempt to fix DB connection issues

--- api/db.go
+++ api/db.go
@@ -13,7 +13,8 @@ type DBContext struct {
 }
 
 func NewDBContext() *DBContext {
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s", os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASS"), os.Getenv("DB_NAME"), os.Getenv("DB_PORT"))
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=America/Chicago", os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"), os.Getenv("DB_PORT"))
+	fmt.Print(dsn)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
